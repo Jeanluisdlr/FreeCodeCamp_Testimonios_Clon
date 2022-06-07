bb97e594b33c703a74e77494eff3e8a99a724571
Jean de la Rosa <jeanluisdelarosa97@gmail.com>
2022-06-07 01:48:22 -0400

    Entire project

---
+++ src/Components/Testimonios.js
@@ -0,0 +1,24 @@
+import React from'react';
+import '../hojas-de-estilo/Testimonio.css';
+
+export function Testimonio(props) {
+  return (
+    <div className='contenedor-testimonio'>
+      <img 
+      className='imagen-testimonio'
+      src={require(`../images/testimonio-${props.imagen}.png`)}
+      alt='foto de coral'/>
+      <div className='contenedor-texto-testimonio'>
+        <p className='nombre-testimonio'>
+          <strong>{props.nombre}</strong> en {props.pais}</p>
+        <p className='cargo-testimonio'>{props.cargo} en  
+        <strong> {props.empresa}</strong></p>
+        <p className='texto-testimonio'>
+          "{props.testimonio}"
+        </p>
+      </div>
+    </div>
+  );
+
+}
+
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root= ReactDOMClient.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  
+);
